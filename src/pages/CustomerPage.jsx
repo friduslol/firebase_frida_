aced67d1acfb6f15ccdd69b3b183269674628f26
frida stenberg <fridastenberg97@hotmail.com>
2022-02-20 13:44:15 +0100

    added styling

--- src/pages/CustomerPage.jsx
+++ src/pages/CustomerPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { useParams, useNavigate } from "react-router-dom"
 import useGetAlbum from "../hooks/useGetAlbum"
 import { addDoc, collection } from 'firebase/firestore'
@@ -22,12 +22,12 @@ const CustomerPage = () => {
     const { id } = useParams()
     const { userId } = useParams()
     const { loading, data: album } = useGetAlbum(id)
+    const myRefs= useRef([]);
     const [imgArr, setImgArr] = useState([])
     const [customerArr, setCustomerArr] = useState([])
     const [removedArr, setRemovedArr] = useState([])
     const [count, setCount] = useState(0)
     const [submit, setSubmit] = useState(false)
-
     useEffect(() => {
         if(album) {
             if(album.images.length) {
@@ -36,22 +36,32 @@ const CustomerPage = () => {
         }
     },[album])
 
-    const addImg = (image) => {
+    useEffect(() => {
+        console.log("customerArr", customerArr)
+    },[customerArr])
+
+    useEffect(() => {
+       console.log("removedArr", removedArr)
+    },[removedArr])
+
+    const addImg = (image, index) => {
        if(customerArr.includes(image) || removedArr.includes(image)) {
             return
        } else {
         setCustomerArr((prevState) => [image, ...prevState])
         setCount((prevState) => prevState += 1)
+        myRefs.current[index].classList.add("add")
        }
 
     }
 
-    const removeImg = (image) => {
+    const removeImg = (image, index) => {
         if(customerArr.includes(image) || removedArr.includes(image)) {
             return
         } else {
             setRemovedArr((prevState) => [image, ...prevState])
             setCount((prevState) => prevState += 1)
+            myRefs.current[index].classList.add("remove");
         }
     }
 
@@ -87,31 +97,19 @@ const CustomerPage = () => {
             >
                 {!imgArr && <p>No Images!</p>}
                 {imgArr && imgArr.map((image, index)=> (
-                <Card className="card" key={index}>
+                <Card className="card" key={index} ref={(el) => (myRefs.current[index] = el)}>
                     <a href={image.url}>
                         <Card.Img variant="top" src={image.url} title={image._id} />
                     </a>
                     <Card.Footer className="card-footer">
-                        <FontAwesomeIcon icon={faThumbsDown} onClick={() => addImg(image)} role="button"/>
-                        <FontAwesomeIcon icon={faThumbsUp} onClick={() => removeImg(image)} role="button"/>
+                        <FontAwesomeIcon icon={faThumbsDown} onClick={() => removeImg(image, index)}
+                        role="button"/>
+                        <FontAwesomeIcon icon={faThumbsUp} onClick={() => addImg(image, index)}
+                        role="button"/>
                     </Card.Footer>
                 </Card>
                 ))}
             </Masonry >
-            <div className="mx-auto customerImgWrapper">
-                <div className="img-wrapper">
-                    <p>{count} / {imgArr.length} Chosen images:</p>
-                    {customerArr.map((image, index) => (
-                        <img style={{ "maxWidth": '5rem' }} key={index} src={image.url} alt="chosen" />
-                    ))}
-                </div>
-                <div className="img-wrapper">
-                    <p>Removed images:</p>
-                    {removedArr.map((image, index) => (
-                        <img style={{ "maxWidth": '5rem' }} key={index} src={image.url} alt="removed" />
-                    ))}
-                </div>
-            </div>
             <div className="btn-wrapper">
                 <Button className="btn" onClick={onReset}>Start over!</Button>
                 <Button className="btn" disabled={submit} onClick={onCreateCopy}>Submit album</Button>
